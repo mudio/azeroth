b98369e6a378be681274de159fe5aa6b19f704c6
木休大人 <523317421@qq.com>
2017-09-22 14:59:37 +0800

    support

--- src/interface/controller.js
+++ src/interface/controller.js
@@ -5,13 +5,64 @@
  * @author mudio(job.mudio@gmail.com)
  */
 
+import cookie from 'cookie';
+
+import {Http302} from '../httpcode';
 import {ControllerCategory} from '../types';
 
 export default class IController {
     constructor(req, res) {
-        this.request = req;
-        this.response = res;
+        this._request = req;
+        this._response = res;
     }
 
     static [ControllerCategory] = true;
+
+    /**
+     * 获取请求的`Cookies`
+     *
+     * @returns Array<string>
+     * @memberof IController
+     */
+    getCookies() {
+        return cookie.parse(this._request.headers.cookie || '');
+    }
+
+    /**
+     * 设置`Response`
+     *
+     * @param {any} Header Key
+     * @param {any} Header Value
+     * @memberof IController
+     */
+    setHeader(key, value) {
+        this._response.setHeader(key, value);
+    }
+
+    /**
+     * 设置`Response` Cookie
+     *
+     * @see https://www.npmjs.com/package/cookie
+     *
+     * @param {any} cookie key
+     * @param {any} cookie value
+     * @param {any} [options={}]
+     * @memberof IController
+     */
+    setCookie(name, value, options = {}) {
+        this._response.setHeader('Set-Cookie', cookie.serialize(name, value, options));
+    }
+
+    /**
+     * 重定向
+     *
+     * @param {string} [location='']
+     * @param {Azeroth.HttpCode} [HttpCode=Http302]
+     * @memberof IController
+     */
+    redirect(location = '/', HttpCode = Http302) {
+        this.setHeader('Location', location);
+
+        throw new HttpCode();
+    }
 }
